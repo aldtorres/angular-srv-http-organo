dde537cc42fb55f52b0135d8da7609dcc98a03de
torrestor <torres.jovem@gmail.com>
2026-01-16 17:42:07 -0300

    erro/exibe popup => interception erro

--- src/app/interceptors/erro.interceptor.ts
+++ src/app/interceptors/erro.interceptor.ts
@@ -12,7 +12,8 @@ export const erroInterceptor: HttpInterceptorFn = (req, next) => {
 
   return next(req).pipe(
     catchError((erro: HttpErrorResponse) => {
-      const mensagemErro = 'Ops, ocorreu um erro - prego -Interceptor'
+      //const mensagemErro = 'Ops, ocorreu um erro - prego -Interceptor'
+      const mensagemErro = obterMensagemDeErro(erro.status)
       console.log(mensagemErro)
       mensagemDeService.mostrarMensagemDeErro(mensagemErro)
       return throwError(() => erro)
@@ -22,11 +23,12 @@ export const erroInterceptor: HttpInterceptorFn = (req, next) => {
 
 function obterMensagemDeErro(status: number): string{
 
-  const mensagemDeErro: Record<number, string> ={
+  const mensagensDeErro: Record<number, string> ={
     0: 'Erro de rede - Não foi possível se conectar ao servidor',
     404: 'Recurso solicitado não encontrado',
     500: 'Erro no servidor. Tente novamente mais tarde'
   }
+  return mensagensDeErro[status] || 'Ocorreu um erro inesperado!'
 }
 
-return mensagensDeErro[status] || 'Ocorreu um erro inesperado!'
+
