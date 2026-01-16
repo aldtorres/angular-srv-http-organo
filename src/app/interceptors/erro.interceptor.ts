30181195cf88a1278c13c39f8f8a7fab62a5bdf6
torrestor <torres.jovem@gmail.com>
2026-01-16 17:34:36 -0300

    erro/exibe popup => interception erro

---
+++ src/app/interceptors/erro.interceptor.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { MensagemErroService } from '../services/mensagem-erro.service';
+
+export const erroInterceptor: HttpInterceptorFn = (req, next) => {
+  //req => REQUEST, 
+  // next => PROXIMA ETAPA
+  //return next(req);
+  
+  const mensagemDeService = inject(MensagemErroService)
+
+  return next(req).pipe(
+    catchError((erro: HttpErrorResponse) => {
+      const mensagemErro = 'Ops, ocorreu um erro - prego -Interceptor'
+      console.log(mensagemErro)
+      mensagemDeService.mostrarMensagemDeErro(mensagemErro)
+      return throwError(() => erro)
+    })
+  );
+};
+
+function obterMensagemDeErro(status: number): string{
+
+  const mensagemDeErro: Record<number, string> ={
+    0: 'Erro de rede - Não foi possível se conectar ao servidor',
+    404: 'Recurso solicitado não encontrado',
+    500: 'Erro no servidor. Tente novamente mais tarde'
+  }
+}
+
+return mensagensDeErro[status] || 'Ocorreu um erro inesperado!'
---
+++ src/app/services/mensagem-erro.service.ts
@@ -0,0 +1,20 @@
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MensagemErroService {
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  mostrarMensagemDeErro(mensagem: string){
+    const config : MatSnackBarConfig = {
+      duration: 3000, //ms
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+    }
+    //exibe um pop top
+    this.snackBar.open(mensagem, 'Fechar', config)
+  }
+}
